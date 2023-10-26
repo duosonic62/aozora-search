1994e185a386b6e07b9e5ebc727e744e02ca557b
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2023-10-26 20:57:23 +0900

    add db schema

--- cmd/database/main.go
+++ cmd/database/main.go
@@ -14,4 +14,15 @@ func main() {
 	}
 
 	defer db.Close()
+	queries := []string{
+		`CREATE TABLE IF NOT EXISTS author(author_id TEXT, author TEXT, PRIMARY KEY (author_id))`,
+		`CREATE TABLE IF NOT EXISTS contents(author_id TEXT, title_id TEXT, title TEXT, PRIMARY KEY (author_id, title_id))`,
+		`CREATE VIRTUAL TABLE IF NOT EXISTS contents_fts USING fts4(words)`,
+	}
+	for _, query := range queries {
+		_, err := db.Exec(query)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
