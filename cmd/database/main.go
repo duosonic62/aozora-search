9974eeab318db9a13e525ca335efdbbe05560c9b
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2023-10-28 09:31:17 +0900

    add entries

--- cmd/database/main.go
+++ cmd/database/main.go
@@ -3,12 +3,33 @@ package main
 import (
 	"database/sql"
 	"log"
+	"strings"
 
+	"github.com/ikawaha/kagome-dict/ipa"
+	"github.com/ikawaha/kagome/v2/tokenizer"
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type Entry struct {
+	AuthorID string
+	Author   string
+	TitleID  string
+	Title    string
+	InfoURL  string
+	ZipURL   string
+}
+
 func main() {
-	db, err := sql.Open("sqlite3", "database.sqlite")
+	db, err := setupDB("database.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// addEntry
+}
+
+func setupDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,4 +46,40 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+
+	return db, nil
+}
+
+func addEntry(db *sql.DB, entry *Entry, content string) error {
+	_, err := db.Exec(`REPLACE INTO authors(author_id, author) values(?, ?)`, entry.AuthorID, entry.Author)
+	if err != nil {
+		return err
+	}
+
+	res, err := db.Exec(`REPLACE INTO contents(author_id, title_id, title, content) values (?, ?, ?, ?)`,
+		entry.AuthorID,
+		entry.TitleID,
+		entry.Title,
+		content,
+	)
+	if err != nil {
+		return err
+	}
+	docID, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	t, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
+	if err != nil {
+		return err
+	}
+
+	seq := t.Wakati(content)
+	_, err = db.Exec(`REPLACE INTO contents_fts(docid, words) values(?, ?)`, docID, strings.Join(seq, " "))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
